2cdfdcde5adc4a70b27aa8f90d525daaab3ba7ed
FeliCarlomagno <feliciano.carlomagno@libero.it>
2023-01-26 16:49:06 +0100

    Add solution

---
+++ libreria.js
@@ -0,0 +1,33 @@
+const fetchReference = async function () {
+  try {
+    let response = await fetch("https://striveschool-api.herokuapp.com/books");
+    console.log(response);
+    if (response.ok) {
+      let data = await response.json();
+      console.log("data", data);
+
+      const cardReference = document.querySelector(".row");
+      data.forEach((book) => {
+        let newCard = document.createElement("div");
+
+        newCard.innerHTML = `<div class="card col col-xxl-3 m-3 w-25" >
+        <img src="${book.img}" class="card-img-top" alt="image poster" />
+        <div class="card-body d-flex flex-column justify-content-center align-items-center bg-light ">
+          <h5 class="card-title" id="cardTitle">${book.title}</h5>
+          <p class="card-text"> Categoria:
+           ${book.category}
+          </p>
+          <p> Prezzo: ${book.price}</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>`;
+
+        cardReference.appendChild(newCard);
+
+        //titleReference.innerText = book.title;
+      });
+    }
+  } catch (error) {}
+};
+
+fetchReference();
